070185587c5b6ae5d2c840a7043eee3435b1ff7d
Reza Karimabadi <rkarimabadi@gmail.com>
2019-02-12 14:38:48 +0330

    اقدام اولیه برای دریافت روزهای معاملاتی

---
+++ TsetmcLib/BaseData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsetmcLib
+{
+    public class BaseData
+    {
+        public static List<ActiveDate> GetActiveTradeDates()
+        {
+            return Enumerable.Empty<ActiveDate>().ToList();
+        }
+    }
+}
